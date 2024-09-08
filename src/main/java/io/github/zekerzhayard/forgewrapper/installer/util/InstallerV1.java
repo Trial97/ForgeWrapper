4f57cae5ef6ffa9aa198be06ac843df9ca1d6261
TheKodeToad <TheKodeToad@proton.me>
2024-09-08 21:28:01 +0100

    Revert "Merge pull request #1 from TheKodeToad/fix-new-forge-installer"
    This reverts commit c2a65dd98f5e405bc7997b1fee62c89a0414fa67, reversing
    changes made to 271bf51f809de6f2f47a1129b86df3a1594e1d6b.

--- src/main/java/io/github/zekerzhayard/forgewrapper/installer/util/InstallerV1.java
+++ src/main/java/io/github/zekerzhayard/forgewrapper/installer/util/InstallerV1.java
@@ -1,9 +1,9 @@
 package io.github.zekerzhayard.forgewrapper.installer.util;
 
 import java.io.File;
-import java.lang.reflect.Method;
+import java.util.function.Predicate;
 
-import net.minecraftforge.installer.actions.PostProcessors;
+import net.minecraftforge.installer.actions.ClientInstall;
 import net.minecraftforge.installer.actions.ProgressCallback;
 import net.minecraftforge.installer.json.Install;
 import net.minecraftforge.installer.json.InstallV1;
@@ -16,22 +16,23 @@ public class InstallerV1 extends AbstractInstaller {
     }
 
     @Override
-    public boolean runClientInstall(Install profile, ProgressCallback monitor, File libraryDir, File minecraftJar,
-            File installerJar) {
-        PostProcessors processors = new PostProcessors(
-                profile instanceof InstallV1 ? (InstallV1) profile : new InstallV1(profile), true, monitor);
+    public boolean runClientInstall(Install profile, ProgressCallback monitor, File libraryDir, File minecraftJar, File installerJar) {
+        return new ClientInstall4MultiMC(profile, monitor, libraryDir, minecraftJar).run(null, input -> true, installerJar);
+    }
 
-        try {
-            Method method = processors.getClass().getMethod("process", File.class, File.class, File.class, File.class);
-            Object result = method.invoke(processors, libraryDir, minecraftJar, libraryDir.getParentFile(),
-                    installerJar);
+    public static class ClientInstall4MultiMC extends ClientInstall {
+        protected File libraryDir;
+        protected File minecraftJar;
 
-            if (method.getReturnType() == boolean.class)
-                return (boolean) result;
+        public ClientInstall4MultiMC(Install profile, ProgressCallback monitor, File libraryDir, File minecraftJar) {
+            super(profile instanceof InstallV1 ? (InstallV1) profile : new InstallV1(profile), monitor);
+            this.libraryDir = libraryDir;
+            this.minecraftJar = minecraftJar;
+        }
 
-            return result != null;
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
+        @Override
+        public boolean run(File target, Predicate<String> optionals, File installer) {
+            return this.processors.process(this.libraryDir, this.minecraftJar, this.libraryDir.getParentFile(), installer);
         }
     }
 }
